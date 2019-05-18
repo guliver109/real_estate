69c91916e80ef39884e728f64b25bda03ab8036e
Marko Gusic <markogusic@aol.com>
2019-05-18 07:23:07 -0700

    add procfile

--- server/server.js
+++ server/server.js
@@ -24,7 +24,8 @@ app.get('/favicon.ico', (req, res, next) => {
     next();
 })
 require('./config/session')(app);
-app.use('/uploads', express.static(path.join(__dirname + '/uploads')));
+// app.use('/uploads', express.static(path.join(__dirname + '/uploads')));
+app.use(express.static(path.join(__dirname, '../client/build')));
 
 app.use(userRoutes);
 app.use('/listings', listingRoutes); // prepending '/listing' to every listing routes
